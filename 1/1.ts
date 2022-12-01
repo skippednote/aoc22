20b45e3aa3e41f2c214e40bc49a711e3c2691369
Bassam Ismail <skippednote@gmail.com>
2022-12-01 21:57:38 +0530

    add tsconfig

--- 1/1.ts
+++ 1/1.ts
@@ -4,12 +4,12 @@ const input: string = fs.readFileSync('./input', 'utf8');
 const calories: number[] = input
   .split('\n\n')
   .map((line) => line.split('\n'))
-  .map((i) => i.reduce((acc, i) => (acc += Number(i)), 0));
+  .map((line) => line.reduce((acc, calorie) => (acc += Number(calorie)), 0));
 
 const highestCalories = Math.max(...calories);
 const totalOfTopThreeCalories = calories
   .sort((a, b) => b - a)
   .slice(0, 3)
-  .reduce((acc, i) => (acc += i));
+  .reduce((acc, calorie) => (acc += calorie));
 
 console.log({ highestCalories, totalOfTopThreeCalories });
