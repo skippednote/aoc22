4511a0c670226ad8612a24ab51eb3ad6380d44bc
Bassam Ismail <skippednote@gmail.com>
2022-12-07 17:03:45 +0530

    generate tokens before creating a tree

--- 7/7.ts
+++ 7/7.ts
@@ -1,32 +1,66 @@
 const fs = require('fs');
 
 const inputs: string[] = fs.readFileSync('./input', 'utf8').split('\n');
-let path: string[] = [];
-const directoryTree: { [key: string]: number } = {};
 
-for (const i of inputs) {
-  const tokens = i.trim().split(' ');
-  if (tokens[1] === 'cd') {
-    if (tokens[2] === '..') {
-      path.pop();
-    } else {
-      path.push(tokens[2]);
+const tokenizer = (inputs: string[]) => {
+  return inputs.map((input) => {
+    const components = input.trim().split(' ');
+    switch (true) {
+      case components[0] === '$' && components[1] === 'cd':
+        return {
+          type: 'cmd',
+          cmd: 'cd',
+          arg: components[2],
+        };
+      case components[0] === '$' && components[1] === 'ls':
+        return {
+          type: 'cmd',
+          cmd: 'ls',
+        };
+      case /^\d/.test(components[0]):
+        return {
+          type: 'file',
+          name: components[1],
+          size: Number(components[0]),
+        };
+      case /^dir/.test(components[0]):
+        return {
+          type: 'dir',
+          name: components[1],
+        };
     }
-  } else if (tokens[1] === 'ls' || tokens[1] === 'dir') {
-    continue;
-  } else {
-    if (/^\d/.test(i)) {
-      path.forEach((_, i) => {
-        const size = Number(tokens[0]);
-        const pathSlice = path.slice(0, i + 1).join('/');
+  });
+};
 
-        directoryTree[pathSlice] = directoryTree[pathSlice]
-          ? directoryTree[pathSlice] + size
-          : size;
+const generateDirectoryTree = (tokens: any) => {
+  const path: string[] = [];
+  const tree: { [key: string]: number } = {};
+
+  for (const token of tokens) {
+    if (token.type === 'cmd') {
+      if (token.cmd === 'ls') {
+        continue;
+      }
+      if (token.arg === '..') {
+        path.pop();
+      } else {
+        path.push(token.arg);
+      }
+    } else if (token.type === 'dir') {
+      continue;
+    } else {
+      path.forEach((_, i) => {
+        const pathSlice = path.slice(0, i + 1).join('/');
+        tree[pathSlice] = tree[pathSlice]
+          ? (tree[pathSlice] += token.size)
+          : token.size;
       });
     }
   }
-}
+  return tree;
+};
+
+const directoryTree = generateDirectoryTree(tokenizer(inputs));
 
 const directoriesLargerThanHundredThousand = () =>
   Object.values(directoryTree)
